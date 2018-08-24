0708668cf5de5285efa8166f7448f543271ea5cd
yenian.ll <yenian.ll@alibaba-inc.com>
2018-08-24 17:10:56 +0800

    一些样式的修改

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom';
 import { Test } from 'components/test/Test';
 import { AppContainer } from 'react-hot-loader';
 
+
 if ((module as any).hot) {
   (module as any).hot.accept(() => {
     ReactDOM.render(
